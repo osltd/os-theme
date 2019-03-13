2e8773b4660343dd81cac1b19c46107d5e835939
David <so2die@hotmail.com>
2019-03-13 13:10:10 +0800

    rebuild for update prod ver.

---
+++ build/precache-manifest.c0db5269897c34dd730af110e4cfc52e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "2d4fd425467881ccc403",
+    "url": "/static/css/main.e067add2.chunk.css"
+  },
+  {
+    "revision": "36c7fd147b7cf0055cda",
+    "url": "/static/js/1.36c7fd14.chunk.js"
+  },
+  {
+    "revision": "c5087d96465d35231e04",
+    "url": "/static/js/2.c5087d96.chunk.js"
+  },
+  {
+    "revision": "e8a8ea3e4e492abf012b",
+    "url": "/static/js/3.e8a8ea3e.chunk.js"
+  },
+  {
+    "revision": "93ec6c8208a6bd1b57cf1e7781766131",
+    "url": "/static/media/icomoon.93ec6c82.svg"
+  },
+  {
+    "revision": "884ab6b2a09bf9b73a70",
+    "url": "/static/js/4.884ab6b2.chunk.js"
+  },
+  {
+    "revision": "4c985cd4869077ff2f15",
+    "url": "/static/js/runtime~main.4c985cd4.js"
+  },
+  {
+    "revision": "2d4fd425467881ccc403",
+    "url": "/static/js/main.2d4fd425.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6d599a1b512c22b286114958bd2b4183",
+    "url": "/static/media/icomoon.6d599a1b.ttf"
+  },
+  {
+    "revision": "f910481c02957d30e48f4ec6a30840de",
+    "url": "/static/media/icomoon.f910481c.eot"
+  },
+  {
+    "revision": "da3ae86565f79c63c9ae65b14a5b0d1c",
+    "url": "/static/media/icomoon.da3ae865.woff"
+  },
+  {
+    "revision": "884ab6b2a09bf9b73a70",
+    "url": "/static/css/4.ff0fe23d.chunk.css"
+  },
+  {
+    "revision": "951cab944de501e1fbcd4f66be110011",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ee686159aeaafa63d617070c4f26143.js"
+  "/precache-manifest.c0db5269897c34dd730af110e4cfc52e.js"
 );
 
 workbox.clientsClaim();
