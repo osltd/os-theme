3ae1c9d4154dfcdd8a18c6ee442a3e6ae395c98c
Lawson Cheng <inbox@lawsoncheng.com>
2020-05-24 00:56:46 +0800

    Adjusted productlist style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation, Link } from 'react-router-dom';
+import {  Link } from 'react-router-dom';
 import './footer.css';
 import { connect } from 'react-redux';
 
