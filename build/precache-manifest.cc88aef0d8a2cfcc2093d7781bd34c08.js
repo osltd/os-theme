a34d0b18c38f68de2fc882af6cab792108c7b2ef
ip yu sze <wendy.ip@gtomato.com>
2021-02-03 01:58:20 +0800

    Update build files

---
+++ build/precache-manifest.cc88aef0d8a2cfcc2093d7781bd34c08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1987f3ff508b9a6ba7d97598a35e09fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2d6e293c87cc822f46c",
+    "url": "/static/css/2.043b003d.chunk.css"
+  },
+  {
+    "revision": "0942cbbd7cc0d26925fa",
+    "url": "/static/css/main.72e77008.chunk.css"
+  },
+  {
+    "revision": "c2d6e293c87cc822f46c",
+    "url": "/static/js/2.23c0e7af.chunk.js"
+  },
+  {
+    "revision": "1155780de505f2c8beff4f97f7759b01",
+    "url": "/static/js/2.23c0e7af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0942cbbd7cc0d26925fa",
+    "url": "/static/js/main.b0f1e1b7.chunk.js"
+  },
+  {
+    "revision": "cc1d5b99f64a1a385f3e",
+    "url": "/static/js/runtime-main.3a75e683.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8de1a5a38a6be80e89dccd5692a90e43.js"
+  "/precache-manifest.cc88aef0d8a2cfcc2093d7781bd34c08.js"
 );
 
 self.addEventListener('message', (event) => {
