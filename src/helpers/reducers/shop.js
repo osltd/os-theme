aedf56e288d9ae3e0d8ce7704e1134dd541be278
Lawson Cheng <inbox@lawsoncheng.com>
2021-07-29 15:11:33 +0800

    adapt settings from attributes to override home_page's layout

--- src/helpers/reducers/shop.js
+++ src/helpers/reducers/shop.js
@@ -8,11 +8,25 @@ let initState = {
 
 export default (state = initState, action) => {
     switch(action.type) {
-        case 'SET_SHOP' :
+        case 'SET_SHOP' : {
+            // preset home page layout
+            let home_page_layout = {}
+            // has home page layout settings?
+            if((action.payload.attributes || {}).home_page_layout != null) {
+                try {
+                    home_page_layout = JSON.parse(action.payload.attributes.home_page_layout)
+                } catch (error) {
+                    // do nothing when error occured
+                }   
+            }
             return {
                 session  : action.payload,
-                settings : OS_Config.settings
+                settings : {
+                    ...OS_Config.settings,
+                    layout_override : home_page_layout
+                }
             };
+        }
         case 'SET_COLLECTIONS' : {
             return {
                 ...state,
